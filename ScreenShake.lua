c9620c9d697929befc78652d94b0f27a49d0f620
AbobaGitHub <112294807+Aboba1001@users.noreply.github.com>
2023-11-01 13:07:39 +0400

    Add files via upload

---
+++ ScreenShake.lua
@@ -0,0 +1,6 @@
+-- Script by ABOBA
+-- YouTube - @RoboaToba
+-- GitHub - Aboba1001
+function opponentNoteHit(a,b,c,isSustainNote)
+  triggerEvent('Screen Shake','0.1, 0.01','0.01, 0.00')
+end -- If you wanna on player instead remove the "opponentNoteHit" and add "goodNoteHit"
